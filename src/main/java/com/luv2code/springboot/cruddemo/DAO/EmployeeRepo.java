8e8b4234421ee3385db11f09665b3223f4e90958
pakizajabeen <76726179+pakizajabeen@users.noreply.github.com>
2021-02-10 14:44:07 +0500

    initial commit

---
+++ src/main/java/com/luv2code/springboot/cruddemo/DAO/EmployeeRepo.java
@@ -0,0 +1,14 @@
+package com.luv2code.springboot.cruddemo.DAO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+	
+	// No need to write code ... LOL! YahoOoOOOOO! 
+
+}
